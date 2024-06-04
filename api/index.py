7e1b109576d5655dd0945b361fe4eec22deec172
Victor Augusto Pereira dos Santos <victorsantosp10@hotmail.com>
2024-06-04 17:19:26 -0300

    fix

--- api/index.py
+++ api/index.py
@@ -3,11 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods = 'POST')
-def soma(a,b):
-    return a+b
-
 def home():
-    c = soma(1,2)
     return "teste"
 
 @app.route('/about')

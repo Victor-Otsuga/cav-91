08fad507844ceb27218d865d43542d3bd1326858
Victor Augusto Pereira dos Santos <victorsantosp10@hotmail.com>
2024-06-04 17:25:31 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,3 +9,17 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/sum', methods=['POST'])
+def sum_values():
+    data = request.get_json()
+    if 'value1' in data and 'value2' in data:
+        value1 = data['value1']
+        value2 = data['value2']
+        sum_result = value1 + value2
+        return jsonify({'sum': sum_result})
+    else:
+        return jsonify({'error': 'Both value1 and value2 are required'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
